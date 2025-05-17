0b7358a16afbbf6aa02923b6a4a8c7a239116418
Vilgiz <mr.igorvarlamov@mail.ru>
2025-05-17 18:33:27 +0300

    Some features

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-
+import FavoritesPage from './pages/FavoritesPage';
 import SearchPage from './pages/SearchPage';
 import BookingsPage from './pages/BookingsPage';
 import AdminPage from './pages/AdminPage';
 import BookingViewPage from './pages/BookingViewPage';
 import NotFound from './pages/NotFound';
-
+import HotelDetailPage from './pages/HotelDetailPage';
 export default function AppRoutes() {
   return (
     <Routes>
@@ -15,6 +15,8 @@ export default function AppRoutes() {
       <Route path="/admin/*" element={<AdminPage />} />
       <Route path="/booking/:id" element={<BookingViewPage />} />
       <Route path="*" element={<NotFound />} />
+      <Route path="/hotels/:id" element={<HotelDetailPage />} />
+      <Route path="/favorites" element={<FavoritesPage />} />
     </Routes>
   );
 }
